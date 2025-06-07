2b7734e61900d45edfdc913fa26e62281a075d2b
Acoidan Martín Conrado <alu0101623627@ull.edu.es>
2025-06-07 14:25:25 +0100

    fix: corregir variables de entorno para la conexión a MySQL

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Conexión a MySQL
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.connect((err) => {
