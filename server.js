e80ca94001e39c06df5ee6bfd1a93a2c1d239e5a
Acoidan Martín Conrado <alu0101623627@ull.edu.es>
2025-06-08 12:13:58 +0100

    fix: actualizar URL del endpoint de envío de tickets de soporte y mejorar configuración de CORS

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://www.calceteam.eu/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
